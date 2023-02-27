caa31b6759cfe736a886cd0673ab90e5ddffa22a
Kadyrbek <Kadyrbek0077@gmail.com>
2023-02-27 10:15:38 +0300

    part first

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Home() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`https://fakestoreapi.com/products`)
+            const data = await response.json()
+            setProducts(data)
+        }
+        fetchData()
+    }, [])
+    console.log(products);
+    return (
+        <div className="container">
+            {/* {products.map(el => <p key={el.id}>{el.title}</p>)} */}
+            {products && products.map((datas) => {
+                return (
+                    // <Link href={`/products/${datas.id}`} key={datas.id}>
+                    <div key={datas.id}>
+                        <h1>{datas.category}</h1>
+                        <img className="image" src={datas.image} alt={datas.category} />
+                        <p className="title">{datas.title}</p>
+                        <p className="price">{datas.price}$</p>
+                    </div>
+
+                    // </Link>
+                )
+            })}
+        </div>
+    );
+}
